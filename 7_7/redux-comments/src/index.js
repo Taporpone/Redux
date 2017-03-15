c26ecbf9804586f84c196aff3daeeadc48c1ca7d
Taporpone <taporpone@gmail.com>
2017-03-15 15:16:50 +0100

    Mod

--- 7_7/redux-comments/src/index.js
+++ 7_7/redux-comments/src/index.js
@@ -2,8 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducer from './reducer';
+import { addComment } from './actions';
+
+const store = createStore(reducer);
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
+
+store.dispatch(addComment('pierwszy komentarz'));
+store.dispatch(addComment('drugi komentarz'));
