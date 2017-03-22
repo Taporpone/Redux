00f876b2e8fb388751ee9f65abb98a7121dd6c31
Taporpone <taporpone@gmail.com>
2017-03-22 13:22:51 +0100

    spacing

--- 7_7/redux-comments/src/Containers/App.js
+++ 7_7/redux-comments/src/Containers/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import CommentsListContainer from './CommentsListContainer';
 
 const App = () => {
-  return(
+  return (
     <div className='App'>
       <CommentsListContainer />
     </div>
